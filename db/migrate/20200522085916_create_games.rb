831469a77dfdaa60ca88f36772caa207e8469829
kuei0221 <michaelhwang0619@gmail.com>
2020-06-10 15:42:59 +0800

    feat(model): create model game and platform

---
+++ db/migrate/20200522085916_create_games.rb
@@ -0,0 +1,12 @@
+class CreateGames < ActiveRecord::Migration[6.0]
+  def change
+    create_table :games do |t|
+      t.string :name
+      t.references :platform, null: false, foreign_key: true
+      t.integer :price
+
+      t.timestamps
+    end
+    add_index :games, :name
+  end
+end

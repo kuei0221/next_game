d601c7d42879397d6db9fbe826d79ae689d7f82b
kuei0221 <michaelhwang0619@gmail.com>
2020-06-10 15:45:02 +0800

    feat(route): move root from home to game, add resource for game

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'games#index'
   devise_for :users, controllers: { sessions: 'users/sessions' }
+
+  # get 'games/show'
+  resources :games, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
 end

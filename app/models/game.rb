325ad9332c6ebcaa07ec9fbf4923b204aef95b69
kuei0221 <michaelhwang0619@gmail.com>
2020-06-12 16:41:24 +0800

    feat(model): add searching scope

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 class Game < ApplicationRecord
   belongs_to :platform
   has_one_attached :cover
+
+  scope :search_by_name, ->(name) { where('name like ?', "%#{name}%") }
+  scope :search_by_platform, ->(id) { where('platform_id': id) }
 end

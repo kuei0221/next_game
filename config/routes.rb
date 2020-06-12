cf0c321fa5c9845dc05c947065cc472ebc211aec
kuei0221 <michaelhwang0619@gmail.com>
2020-06-12 16:40:01 +0800

    feat(config): add index for games in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   root to: 'games#index'
   devise_for :users, controllers: { sessions: 'users/sessions' }
 
-  # get 'games/show'
-  resources :games, only: :show
+  resources :games, only: %i[show index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

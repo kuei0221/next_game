edf678aba359a5da44b6a5cd3c9bd1be06a882a5
kuei0221 <michaelhwang0619@gmail.com>
2020-06-12 16:44:14 +0800

    feat(controller): allow query string to search game in games#controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,27 @@
-class GamesController < ApplicationController
-  def index
-    @games = Game.all.includes(:platform)
-  end
+# frozen_string_literal: true
 
+class GamesController < ApplicationController
   def show
     @game = Game.find_by(id: params[:id])
+  end
+
+  def index
+    @games = Game.all
+
+    if game_params[:name]
+      @games = @games.search_by_name(game_params[:name])
+    end
+
+    if game_params[:platform]
+      @games = @games.search_by_platform(game_params[:platform])
+    end
+
+    @games = @games.with_attached_cover.includes(:platform)
+  end
+
+  private
+
+  def game_params
+    params.permit(:name, :platform, :commit)
   end
 end

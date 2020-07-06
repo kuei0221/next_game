f5e9636fb4e1474d2f01cfcbd2618708eb61af52
kuei0221 <michaelhwang0619@gmail.com>
2020-07-06 15:50:50 +0800

    feat(model): add function to take lowest price , best choice from stocks

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,8 +3,20 @@
 class Game < ApplicationRecord
   belongs_to :platform
   has_one_attached :cover
-  has_many :stocks
+  has_many :stocks, -> { selling.order(price: :asc, updated_at: :desc) }
 
   scope :search_by_name, ->(name) { where('name like ?', "%#{name}%") }
   scope :search_by_platform, ->(id) { where('platform_id': id) }
+
+  def lowest_price
+    stocks.any? ? stocks.first.price : price
+  end
+
+  def best_available_stock(user_id)
+    stocks.not_own_by_current_user(user_id).first
+  end
+
+  def total_quantity
+    stocks.sum(:quantity)
+  end
 end

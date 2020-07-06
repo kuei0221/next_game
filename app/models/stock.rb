5668c79fde2b5bacfb3ef924c2515d6bb6f33e53
kuei0221 <michaelhwang0619@gmail.com>
2020-07-06 15:50:08 +0800

    feat(model): add scope to avoid select own stock

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,11 +3,11 @@
 class Stock < ApplicationRecord
   include AASM
 
-  belongs_to :game
+  belongs_to :game, dependent: :destroy
   belongs_to :user, dependent: :destroy
+  scope :not_own_by_current_user, ->(user_id) { where.not(user_id: user_id) }
 
-  validates :user_id, uniqueness: { scope: :game_id, message: 'already has the stock of this game'}
-
+  validates :user_id, uniqueness: { scope: :game_id, message: 'already has the stock of this game' }
   delegate :id, :name, :price, to: :game, prefix: true
 
   enum state: { pending: 0, selling: 1 }

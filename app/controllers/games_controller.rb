c2ca677b095ffcea694142c8fd3f3d5d69df3fea
kuei0221 <michaelhwang0619@gmail.com>
2020-07-06 16:01:22 +0800

    feat(controller): game now include stock

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 class GamesController < ApplicationController
+
   def show
-    @game = Game.find_by(id: params[:id])
+    @game = Game.includes(stocks: :user).find_by(id: params[:id])
+    unless @game
+      flash.alert = 'Game not Found'
+      redirect_to games_path
+    end
   end
 
   def index
@@ -16,7 +21,7 @@ class GamesController < ApplicationController
       @games = @games.search_by_platform(game_params[:platform])
     end
 
-    @games = @games.with_attached_cover.includes(:platform)
+    @games = @games.with_attached_cover.includes(:platform, :stocks)
   end
 
   private

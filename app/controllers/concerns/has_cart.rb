5694ee53e74840df0680bcfc4fe630efc3f3a102
kuei0221 <michaelhwang0619@gmail.com>
2020-07-07 18:26:35 +0800

    feat(controller/concern): add function to get current_cart

---
+++ app/controllers/concerns/has_cart.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module HasCart
+  extend ActiveSupport::Concern
+
+  included do
+    helper_method :current_cart
+
+    private
+
+    def current_cart
+      @current_cart ||= Cart.new(current_user.id)
+    end
+  end
+end

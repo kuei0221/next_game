c2d1fc655336b8d374af0bb914eb22c9b6466a2c
michaelhwang <michaelhwang0619@gmail.com>
2020-08-04 18:35:58 +0800

    feat(config): remove mailgun setting in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,6 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
   # Show full error reports.
   config.consider_all_requests_local = true
 
@@ -38,11 +37,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = {
-    api_key: ENV['MAILGUN_API_KEY'],
-    domain: ENV['MAILGUN_DOMAIN'],
-  }
+  
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log

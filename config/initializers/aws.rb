43c44d029ec42135b33cfb2009e8cb041e0f1fe1
michaelhwang <michaelhwang0619@gmail.com>
2020-08-04 18:26:28 +0800

    feat(config): add aws.rb to load credential setting when init

---
+++ config/initializers/aws.rb
@@ -0,0 +1,8 @@
+require "aws-sdk-s3"
+
+credential = Aws::Credentials.new(
+  access_key_id= Rails.application.credentials.dig(:aws, :access_key_id),
+  secret_access_key= Rails.application.credentials.dig(:aws, :secret_access_key)
+)
+
+Aws.config.update({region: ENV['AWS_REGION'], credentials: credential})

21fd36b51856df454c3855d7fa2f01c83b16a73d
michaelhwang <michaelhwang0619@gmail.com>
2020-09-10 15:29:44 +0800

    feat(model): allow stock and order_item to inherit from product

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Order < ApplicationRecord
-  has_many :items, class_name: 'OrderItem', foreign_key: :order_uuid, inverse_of: :order
+  has_many :items, class_name: 'OrderItem', foreign_key: :order_id, inverse_of: :order
   belongs_to :buyer, class_name: "User", foreign_key: "buyer_id"
 
   attribute :uuid, :string, default: -> { SecureRandom.uuid }
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,33 +1,12 @@
 # frozen_string_literal: true
 
-class Stock < ApplicationRecord
-  include AASM
-
-  belongs_to :game, dependent: :destroy
-  belongs_to :user, dependent: :destroy
+class Stock < Product
+  
   scope :not_own_by_current_user, ->(user_id) { where.not(user_id: user_id) }
-
-  validates :user_id, uniqueness: { scope: :game_id, message: 'already has the stock of this game' }
-  delegate :id, :name, :price, to: :game, prefix: true
-
-  enum state: { pending: 0, selling: 1, shipping: 2, sold: 3}
-
-  attribute :type, default: 'Stock'
-
-  aasm column: :state, enum: true do
-    state :pending, initial: true
-    state :selling
-    state :shipping
-    state :sold
-
-    event :open do
-      transitions from: :pending, to: :selling
-    end
-
-    event :close do
-      transitions from: :selling, to: :pending
-    end
-  end
+  
+  validates_uniqueness_of :user_id, scope: :game_id, message: 'already has the stock of this game'
+  
+  attribute :state, default: :pending
 
   def is_users?(user_id)
     
@@ -35,8 +14,9 @@ class Stock < ApplicationRecord
   end
   
   def reduce_quantity!(num)
-    raise ArgumentError unless num > 0
+    raise ArgumentError unless num > 0 && quantity >= num
 
     decrement!(:quantity, num)
+    self.quantity
   end
 end

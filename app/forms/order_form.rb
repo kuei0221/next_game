6d02028e9893f47cdcb589cb1845108820705797
michaelhwang <michaelhwang0619@gmail.com>
2020-09-13 16:07:11 +0800

    refactor(form): change order into instance variable

--- app/forms/order_form.rb
+++ app/forms/order_form.rb
@@ -4,7 +4,7 @@ class OrderForm
 
   include ActiveModel::Model
 
-  attr_accessor :user_id, :cart
+  attr_accessor :user_id, :cart, :order
   delegate :cart_items, :total_price, to: :cart
 
   def initialize(user_id)
@@ -14,14 +14,14 @@ class OrderForm
 
   def save
     ActiveRecord::Base.transaction do
-      order = Order.new(
+      @order = Order.new(
         buyer_id: user_id,
         price: cart.total_price
       )
       lock_stocks
       cart.checkout!
-      order.items = cart.cart_items.map(&:to_order_item)
-      order.save!
+      @order.items = cart.cart_items.map(&:to_order_item)
+      @order.save!
       cart.clear!
     end
   rescue Cart::CheckoutError => e

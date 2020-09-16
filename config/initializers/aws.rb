32b891c0dc6babff2bd7c321c9ba2794ca09b38a
michaelhwang <michaelhwang0619@gmail.com>
2020-09-16 15:23:26 +0800

    feat(config): style update

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,10 @@
-require "aws-sdk-s3"
+# frozen_string_literal: true
+
+require 'aws-sdk-s3'
 
 credential = Aws::Credentials.new(
-  access_key_id= Rails.application.credentials.dig(:aws, :access_key_id),
-  secret_access_key= Rails.application.credentials.dig(:aws, :secret_access_key)
+  access_key_id = Rails.application.credentials.dig(:aws, :access_key_id),
+  secret_access_key = Rails.application.credentials.dig(:aws, :secret_access_key)
 )
 
-Aws.config.update({region: ENV['AWS_REGION'], credentials: credential})
+Aws.config.update({ region: ENV['AWS_REGION'], credentials: credential })
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 require 'connection_pool'
 Redis::Objects.redis = ConnectionPool.new(size: 5, timeout: 5) { Redis.new(url: ENV['REDIS_URL']) }
